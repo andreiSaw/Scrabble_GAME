7edc8f2d3e057d3e77b5e0a4ceeb218961dd5763
andreiSaw <ias96@list.ru>
2016-04-30 19:28:50 +0300

    30th of April 19 28 one player game

--- app/src/main/java/com/scrabble/GameActivity.java
+++ app/src/main/java/com/scrabble/GameActivity.java
@@ -1,17 +1,20 @@
 package com.scrabble;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.util.List;
 import java.util.Objects;
+import java.util.Vector;
 
 public class GameActivity extends AppCompatActivity {
+
     int[] bottomLineIDs = new int[7];
     Trie trie;
     Dictionary dic = new Dictionary();
@@ -33,35 +36,150 @@ public class GameActivity extends AppCompatActivity {
 
     }
 
-    private void loadSubmitButton() {
-        Button btn=new Button(this);
-        btn.setText("S U B M I T");
-        btn.setBackgroundResource(R.drawable.button_success_selector);
-        RelativeLayout relativeLayout=(RelativeLayout)findViewById(R.id.secondRelativeLayout);
-        RelativeLayout.LayoutParams params=new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.FILL_PARENT,RelativeLayout.LayoutParams.WRAP_CONTENT);
-        params.addRule(RelativeLayout.CENTER_HORIZONTAL);
-        params.addRule(RelativeLayout.ABOVE,bottomLineIDs[POOL_SIZE/2]);
-        btn.setLayoutParams(params);
-        relativeLayout.addView(btn);
-        //checking
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MyButtton x;
-                for(int i=0;i<POOL_SIZE;++i)
-                {
-                    for(int j=0;j<POOL_SIZE;++j)
-                    {
-                        x=(MyButtton)findViewById(masOfIDs[i][j]);
-                        if(!x.isEmpty()&&!x.isLocked())
-                        {
-                            x.setLocked(true);
+    private Button.OnClickListener submitButtonListener = new Button.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Vector<Integer> cols = new Vector<>(), rows = new Vector<>();
+            MyButtton x;
+            Vector<MyButtton> myButttons = new Vector<>();
+            String word = "";
+            int curColumn, curRow;
+            int columnStarts = 0, columnEnds = 0, rowStarts = 0, rowEnds = 0;
+            //i - is row
+            //j - is column
+            for (int i = 0; i < POOL_SIZE; ++i) {
+                for (int j = 0; j < POOL_SIZE; ++j) {
+                    x = (MyButtton) findViewById(masOfIDs[i][j]);
+                    if (!x.isEmpty() && !x.isLocked()) {
+                        if (!cols.contains(j)) {
+                            cols.add(j);
+                        }
+                        if (!rows.contains(i)) {
+                            rows.add(i);
                         }
                     }
                 }
             }
-        });
+
+            //curRow = 0;
+//look over each column
+            // downward
+            for (int i : cols) {
+                curColumn = i;
+                curRow = 0;
+                x = (MyButtton) findViewById(masOfIDs[curRow][curColumn]);
+                //find where word starts
+                while (curRow != POOL_SIZE) {
+                    x = (MyButtton) findViewById(masOfIDs[curRow][curColumn]);
+                    if (x.isEmpty()) {
+                        ++curRow;
+                        continue;
+                    } else {
+                        rowStarts = curRow;
+                        break;
+                    }
+                }
+                //find where word ends
+                while (!x.isEmpty() && curRow != POOL_SIZE) {
+                    myButttons.add(x);
+                    word += x.getText();
+                    ++curRow;
+                    if (curRow == POOL_SIZE) {
+                        break;
+                    }
+                    x = (MyButtton) findViewById(masOfIDs[curRow][curColumn]);
+                }
+                rowEnds = curRow - 1;
+                //checking
+                if (word.length() > 2) {
+                    List myList = trie.getWords(word);
+                    if (myList.isEmpty()) {
+                        word = "";
+                        myButttons.removeAllElements();
+                    } else {
+                        for (MyButtton y : myButttons) {
+                            y.setLocked(true);
+                        }
+                        word = "";
+                        myButttons.removeAllElements();
+                    }
+                } else {
+                    word = "";
+                    myButttons.removeAllElements();
+                }
+
+            }
+//look over each row
+            //rightward
+            // curColumn = 0;
+            for (int i : rows) {
+                curRow = i;
+                curColumn = 0;
+                x = (MyButtton) findViewById(masOfIDs[curRow][curColumn]);
+
+                //find where word starts
+                while (curColumn != POOL_SIZE) {
+                    x = (MyButtton) findViewById(masOfIDs[curRow][curColumn]);
+                    if (x.isEmpty()) {
+                        ++curColumn;
+                        continue;
+                    } else {
+                        columnStarts = curColumn;
+                        break;
+                    }
+                }
+                //find where word ends
+                while (!x.isEmpty() && curColumn != POOL_SIZE) {
+                    myButttons.add(x);
+                    word += x.getText();
+                    ++curColumn;
+
+                    if (curColumn == POOL_SIZE) {
+                        break;
+                    }
+
+                    x = (MyButtton) findViewById(masOfIDs[curRow][curColumn]);
+                }
+                columnEnds = curColumn - 1;
+
+                //checking
+                if (word.length() > 2) {
+                    List myList = trie.getWords(word);
+                    //delete word from trie
+                    if (myList.isEmpty()) {
+                        word = "";
+                        myButttons.removeAllElements();
+                    } else {
+                        for (MyButtton y : myButttons) {
+                            y.setLocked(true);
+                        }
+                        word = "";
+                        myButttons.removeAllElements();
+                    }
+                } else {
+                    word = "";
+                    myButttons.removeAllElements();
+                }
+            }
+        //ask new word from trie to bottom (rack)
+        }
+    };
+
+    private void loadSubmitButton() {
+        Button btn = new Button(this);
+        btn.setText("S U B M I T");
+        btn.setBackgroundResource(R.drawable.button_success_selector);
+        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.secondRelativeLayout);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.FILL_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        params.addRule(RelativeLayout.ABOVE, bottomLineIDs[POOL_SIZE / 2]);
+        btn.setLayoutParams(params);
+        relativeLayout.addView(btn);
+        //checking
+        btn.setOnClickListener(submitButtonListener);
+
     }
+
 
     private void game() {
         //
@@ -197,6 +315,8 @@ public class GameActivity extends AppCompatActivity {
                     params.addRule(RelativeLayout.ALIGN_PARENT_START);
                     bt.setLayoutParams(params);
                     bt.setOnPool(true);
+                    bt.setLocked(false);
+                    bt.setEmpty(true);
                 }
             }
         }
